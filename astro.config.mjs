e463d6645a72379c0d5568714a365fd553101789
Mirzam1n5 <milanamelnikova1307@gmail.com>
2026-03-31 22:40:29 +0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://mirzam.dev', // замени на свой домен или vercel url
+  site: 'https://1n5blog.vercel.app/',
 });
